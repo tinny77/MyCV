a5a5b5beb9621285d90d3a75ff0a6fc77690e9c8
tinny77 <tinny77@gmail.com>
2023-11-22 15:13:24 +0100

    Bio update

--- src/App.jsx
+++ src/App.jsx
@@ -89,22 +89,19 @@ function App() {
 						for detail and a knack for creating seamless, user-friendly
 						websites.
 						<br />
-						Over time, I transitioned my focus towards WordPress, honing my
-						skills in front-end technologies. From customizing existing themes
-						to developing new ones, I've been deeply involved in the WordPress
-						ecosystem.
+						Over time, I transitioned my focus towards WordPress, while honing
+						my skills in front-end technologies. From customizing (namely,
+						turning them upside-down) existing themes to developing new ones
+						from scratch, I've been deeply involved in the WordPress ecosystem
+						for the past few years.
 					</p>
 					<p className="text-block">
-						In 2023, I took a leap, temporarily stepping away to dedicate myself
-						to an intensive period of learning. During this period of full-time
-						commitment, I immersed myself in the latest knowledge and trends,
-						delving deep into the realm of JavaScript, particularly focusing on
-						the nuances of React and related technologies.
-						<br />
-						This dedicated period of study allowed me to not only catch up but
-						to surge ahead, gaining a comprehensive understanding of the
-						ever-evolving landscape of web development while honing my skills and delving into the
-						intricacies that shape the digital world.
+						In 2023, I halted all other engagements to commit fully to an
+						intensive period of learning. I focused solely on mastering
+						JavaScript, particularly delving into React and related
+						technologies. This immersive dedication allowed me to swiftly grasp
+						the dynamic landscape of web development, significantly advancing my
+						skills and expertise in shaping the digital realm.
 					</p>
 					<p className="text-block">
 						Beyond the screen, you'll often find me hitting the gym, or taking
