6ee63fec72504ec5cf5a80b854af9c8e9577401c
tinny77 <tinny77@gmail.com>
2023-12-01 12:45:24 +0100

    Added icons related to experience

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,65 +1,105 @@
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import ScrollAnimation from 'react-animate-on-scroll';
 
 export default function Experience() {
 	return (
 		<>
 			<Container fluid="lg">
 				<Col xs={12}>
-						<Card>
+					<Card>
+						<Card.Body>
+							<Card.Title>
+								Wordpress Developer <span>Creativart (2023)</span>
+							</Card.Title>
 							<Card.Body>
-								<Card.Title>Wordpress Developer <span>Creativart (2023)</span></Card.Title>
-								<Card.Body>
-									Encompassed a range of WordPress-specific tasks, including
-									theme creation and customization using PHP, ACF, Elementor,
-									SEO optimization, and speed enhancement. Additionally, managed
-									the entire content transfer process from the development
-									environment to production, handling all necessary
-									configurations, including server-side setups.
-								</Card.Body>
+								Encompassed a range of WordPress-specific tasks, including theme
+								creation and customization using PHP, ACF, Elementor, SEO
+								optimization, and speed enhancement. Additionally, managed the
+								entire content transfer process from the development environment
+								to production, handling all necessary configurations, including
+								server-side setups.
 							</Card.Body>
-						</Card>
+							<Card.Footer>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+								<i class="devicon-sass-original colored"></i>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+								<i class="devicon-wordpress-plain "></i>
+								<i class="devicon-php-plain colored"></i>
+								<i class="devicon-mysql-plain "></i>
+								<i class="devicon-git-plain colored"></i>
+								<i class="devicon-gulp-plain colored"></i>
+								<i class="devicon-docker-plain colored"></i>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg" />
+								<i class="devicon-xd-plain colored"></i>
+							</Card.Footer>
+						</Card.Body>
+					</Card>
 				</Col>
 				<Col xs={12}>
-
-						<Card>
+					<Card>
+						<Card.Body>
+							<Card.Title>
+								Front-end and Wordpress Developer{' '}
+								<span>24 Consulting (2018-2023)</span>
+							</Card.Title>
 							<Card.Body>
-								<Card.Title>
-									Front-end and Wordpress Developer <span>24 Consulting (2018-2023)</span>
-								</Card.Title>
-								<Card.Body>
-									Created custom themes using PHP and ACF. Demonstrated
-									proficiency in developing static websites, leveraging bundlers
-									and generators for seamless integration. Ensured responsive
-									designs through CSS, HTML, and JavaScript, and oversaw site
-									maintenance and optimization, all seamlessly managed through
-									Git. Extensively worked with design layouts received from
-									Adobe XD and InDesign. Frequently utilized Photoshop for
-									fine-tuning and optimizing design elements, ensuring a
-									pixel-perfect final product.
-								</Card.Body>
+								Created custom themes using PHP and ACF. Demonstrated
+								proficiency in developing static websites, leveraging bundlers
+								and generators for seamless integration. Ensured responsive
+								designs through CSS, HTML, and JavaScript, and oversaw site
+								maintenance and optimization, all seamlessly managed through
+								Git. Extensively worked with design layouts received from Adobe
+								XD and InDesign. Frequently utilized Photoshop for fine-tuning
+								and optimizing design elements, ensuring a pixel-perfect final
+								product.
 							</Card.Body>
-						</Card>
-
+							<Card.Footer>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+								<i class="devicon-sass-original colored colored"></i>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+								<i class="devicon-wordpress-plain"></i>
+								<i class="devicon-php-plain colored"></i>
+								<i class="devicon-mysql-plain "></i>
+								<i class="devicon-git-plain colored"></i>
+								<i class="devicon-gulp-plain colored"></i>
+								<i class="devicon-npm-original-wordmark colored"></i>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg" />
+								<i class="devicon-xd-plain colored"></i>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+							</Card.Footer>
+						</Card.Body>
+					</Card>
 				</Col>
 				<Col xs={12}>
-
-						<Card>
+					<Card>
+						<Card.Body>
+							<Card.Title>
+								Front-end developer <span>24 Consulting (2006-2018)</span>
+							</Card.Title>
 							<Card.Body>
-								<Card.Title>
-									Front-end developer <span>24 Consulting (2006-2018)</span>
-								</Card.Title>
-								<Card.Body>
-									Created responsive web interfaces using self taught CSS, HTML,
-									and JavaScript. Managed multimedia elements for UX and
-									developed back-end solutions with ASP and SQL for seamless
-									data integration.
-								</Card.Body>
+								Created responsive web interfaces using self taught CSS, HTML,
+								and JavaScript. Managed multimedia elements for UX and developed
+								back-end solutions with ASP and SQL for seamless data
+								integration.
 							</Card.Body>
-						</Card>
-
+							<Card.Footer>
+								<i class="devicon-ie10-original colored"></i>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+								<i class="devicon-jquery-plain-wordmark"></i>
+								<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg" />
+								<i class="devicon-sqlite-plain-wordmark"></i>
+							</Card.Footer>
+						</Card.Body>
+					</Card>
 				</Col>
 			</Container>
 		</>
